233c85e1c36676daa947a3dbd694cd9875d3edc9
Tesfit12 <menkregrma12@gmail.com>
2022-07-28 18:52:41 -0700

    two files added

---
+++ FileOne.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function FileOne() {
+    return (
+        <div>FileOne</div>
+
+    )
+}
+
+export default FileOne
---
+++ FileTwo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FileTwo() {
+    return (
+        <>
+            <div>FileTwo</div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adip
+            </p>
+        </>
+    )
+}
+
+export default FileTwo
