c2484b1663f022bb49feab599c8c8b4f702b66c5
Tesfit12 <menkregrma12@gmail.com>
2022-07-28 19:30:52 -0700

    cccc one

--- FileOne.jsx
+++ FileOne.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 function FileOne() {
 
-  
+
     return (
         <div>FileOne</div>
 
     )
 }
 
-console.log("First change from GitHub ...")
 
 export default FileOne
--- FileTwo.jsx
+++ FileTwo.jsx
@@ -2,14 +2,6 @@ import React from 'react'
 
 function FileTwo() {
 
-    const funcTwo = () => {
-        console.log('funcTwo has added from git .... ')
-    }
-
-    const handleAddForm = () => {
-        console.log('hello from handleAddForm...')
-    }
-
     return (
         <>
             <div>FileTwo</div>
